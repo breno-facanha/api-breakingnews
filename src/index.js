415e61d0443ddf9579d9aec2ffe8b01ce5e47198
Breno Façanha <brenofacanha.ti@gmail.com>
2025-03-12 07:31:28 -0300

    adicionando que o projeto receba e interprete json e criando post user no usercontroller

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const userRoute = require("./routes/user.route")
 
 const port = 3100
 
+app.use(express.json())
 app.use("/user", userRoute)
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
