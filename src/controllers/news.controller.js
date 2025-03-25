fc4a6e6b962e91467d41f00fead8d1f85fe5b13f
Breno Façanha <brenofacanha.ti@gmail.com>
2025-03-25 15:20:41 -0300

    adicionando paginação em ordem decrescente das news

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -1,4 +1,4 @@
-import { createService, findAllService } from "../services/news.service.js";
+import { createService, findAllService, countNews } from "../services/news.service.js";
 
 const create = async (req, res) => {
   try {
@@ -23,7 +23,27 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req, res) => {
-  const news = await findAllService();
+  let {limit, offset} = req.query
+
+  limit = +limit
+  offset = +offset
+
+  if(!limit){
+    limit = 5
+  }
+  
+  if(!offset){
+    offset = 0
+  }
+  const news = await findAllService(offset, limit);
+  const total = await countNews()
+  const currentUrl = req.baseUrl
+
+  const next = offset + limit;
+  const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+  const previous = offset - limit < 0 ? null: offset - limit
+  const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}`: null
 
   if (news.length === 0) {
     return res.status(400).send({
@@ -31,7 +51,25 @@ const findAll = async (req, res) => {
     });
   }
 
-  res.send(news);
+  res.send({
+    nextUrl,
+    previousUrl,
+    limit,
+    offset,
+    total,
+
+    results: news.map((newsItem) => ({
+      id: newsItem._id,
+      title: newsItem.title,
+      text: newsItem.text,
+      banner: newsItem.banner,
+      likes: newsItem.likes,
+      comments: newsItem.comments,
+      name: newsItem.user.name,
+      userName: newsItem.username,
+      userAvatar: newsItem.user.userAvatar
+    }))
+  });
 };
 
 export { create, findAll };
