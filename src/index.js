d1db3265f8ea07c16296fcfff7135efdd6e47da8
Breno Façanha <brenofacanha.ti@gmail.com>
2025-04-08 11:56:45 -0300

    instalado a biblioteca cors para consumo do front end

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import connectDatabase from './database/db.js'
 import dotenv from "dotenv"
+import cors from "cors"
 
 import userRoute from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
@@ -13,6 +14,7 @@ const app = express();
 
 const port = process.env.PORT || 3100
 connectDatabase()
+app.use(cors())
 app.use(express.json())
 app.use("/user", userRoute)
 app.use("/auth", authRoute)
