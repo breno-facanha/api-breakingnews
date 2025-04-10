6031dddbd1aa3eaf75eadcfc2652095159bb4124
Breno Façanha <brenofacanha.ti@gmail.com>
2025-04-10 14:46:56 -0300

    desfazendo erro jwt

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import connectDatabase from './database/db.js'
 import dotenv from "dotenv"
-import cors from "cors"
 
 import userRoute from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
@@ -14,7 +13,6 @@ const app = express();
 
 const port = process.env.PORT || 3100
 connectDatabase()
-app.use(cors())
 app.use(express.json())
 app.use("/user", userRoute)
 app.use("/auth", authRoute)
