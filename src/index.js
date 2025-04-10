cd835ed139b70240893569cea70166c886003135
Breno Façanha <brenofacanha.ti@gmail.com>
2025-04-10 14:55:51 -0300

    adicionando cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import connectDatabase from './database/db.js'
 import dotenv from "dotenv"
-
+import cors from "cors"
 import userRoute from './routes/user.route.js'
 import authRoute from './routes/auth.route.js'
 import newsRoute from './routes/news.route.js'
@@ -12,6 +12,7 @@ dotenv.config();
 const app = express();
 
 const port = process.env.PORT || 3100
+app.use(cors())
 connectDatabase()
 app.use(express.json())
 app.use("/user", userRoute)
